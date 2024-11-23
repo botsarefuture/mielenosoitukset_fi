84248f6bc6fad97b24882f452603ab6d3e8435bf
botsarefuture <vuoreol@pm.me>
2024-11-23 14:51:26 +0200

    Add detailed docstring to main function and create VERSION file

--- run.py
+++ run.py
@@ -5,6 +5,22 @@ app = create_app()
 
 
 def main():
+    """
+    The main entry point for the application.
+
+    This function retrieves configuration settings for the application,
+    updates the application's configuration to set the default language
+    to Finnish, and then runs the application.
+
+    Configuration settings:
+    - PORT: The port number on which the application will run. Defaults to 5000 if not set.
+    - DEBUG: The debug mode for the application. Defaults to False if not set.
+
+    The application's configuration is updated to set the default locale to Finnish
+    and to support only the Finnish language.
+
+    The application is then started with the specified debug mode and port number.
+    """
     """ """
     # Retrieve configurations with fallback defaults
     port = int(os.getenv("PORT", app.config.get("PORT", 5000)))

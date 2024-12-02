fb4dc97617a17e29b286d602d07dfa58c433bd85
botsarefuture <vuoreol@pm.me>
2024-12-02 10:56:33 +0200

    Refactor organization handling and enhance view template; implement organization class improvements, update social media links display, and add user management features

--- mielenosoitukset_fi/utils/classes/Organization.py
+++ mielenosoitukset_fi/utils/classes/Organization.py
@@ -1,15 +1,16 @@
-from typing import Dict, List
+from typing import Dict, List, Union
 from bson import ObjectId
 from mielenosoitukset_fi.utils.classes.BaseModel import BaseModel
 from mielenosoitukset_fi.users.models import User
-from .MemberShip import MemberShip
+from mielenosoitukset_fi.utils.classes.MemberShip import MemberShip as Membership
 from mielenosoitukset_fi.utils.database import get_database_manager
 
 DB = get_database_manager()
 
+from mielenosoitukset_fi.utils.classes.Organizer import BaseEntity
 
-class Organization(BaseModel):
-    """ """
+class Organization(BaseEntity):
+    """Class representing an organization."""
 
     def __init__(
         self,
@@ -23,14 +24,11 @@ class Organization(BaseModel):
         _id=None,
         invitations=None,
     ):
-        self.name = name
+        super().__init__(name, email, website, _id)
         self.description = description
-        self.email = email
-        self.website = website
         self.social_media_links = social_media_links or {}
         self.members = members or []
         self.verified = verified
-        self._id = _id or ObjectId()
         self.invitations = invitations or []
 
         self.init_members()
@@ -38,7 +36,7 @@ class Organization(BaseModel):
     def init_members(self):
         """Initialize the members list."""
         self.members = [
-            User.from_db(DB["users"].find_one({"_id": ObjectId(member["user_id"])}))
+            User.from_OID(ObjectId(member["user_id"]))
             for member in self.members
         ]
 
@@ -84,16 +82,11 @@ class Organization(BaseModel):
         ------
         TypeError
             If the email is not a string.
-
-
         """
-
         if not isinstance(email, str):
             raise TypeError("Email must be a string.")
 
         return any(member.email == email for member in self.members)
-
-    from typing import Union
 
     def add_member(self, member: Union[ObjectId, User], role="member", permissions=[]):
         """Add a member to the organization.

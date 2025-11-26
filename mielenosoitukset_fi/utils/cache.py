3304ec932886671e07bb3171d90f5a8665b919e8
botsarefuture <vuoreol@pm.me>
2025-11-26 17:41:54 +0200

    Changed cache config into config file

--- mielenosoitukset_fi/utils/cache.py
+++ mielenosoitukset_fi/utils/cache.py
@@ -1,9 +1,11 @@
 from flask_caching import Cache
 
+from config import Config
+
 cache = Cache(config={
-        "CACHE_TYPE": "RedisCache",
-        "CACHE_REDIS_HOST": "localhost",   # or your Redis host
-        "CACHE_REDIS_PORT": 6379,          # default Redis port
-        "CACHE_REDIS_DB": 0,               # default DB
-        "CACHE_DEFAULT_TIMEOUT": 600       # 10 minutes
+        "CACHE_TYPE": Config.CACHE_TYPE,
+        "CACHE_REDIS_HOST": Config.CACHE_REDIS_HOST,
+        "CACHE_REDIS_PORT": Config.CACHE_REDIS_PORT,
+        "CACHE_REDIS_DB": Config.CACHE_REDIS_DB,
+        "CACHE_DEFAULT_TIMEOUT": Config.CACHE_DEFAULT_TIMEOUT
     })
